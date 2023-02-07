6a0b218b68fc57e2a3ab2b72b89fa4b0b12204ed
askeladd654 <123724414+askeladd654@users.noreply.github.com>
2023-02-07 16:57:30 -0500

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -321,7 +321,7 @@ else:
 BASE_URL = environ.get('BASE_URL', '').rstrip("/")
 if len(BASE_URL) == 0:
     warning('BASE_URL not provided!')
-    BASE_URL = 'https://supernovapro1.onrender.com'
+    BASE_URL = 'https://thefutureisogly.onrender.com'
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
